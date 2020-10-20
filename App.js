209ce227c1299564585c7f64ec13d8bfe39ad6dd
renanxd25 <renan199x@gmail.com>
2020-10-20 11:35:05 -0300

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { View,Text, StyleSheet } from 'react-native';
+import Comments from './src/components/coments.js';
+import Header from './src/components/header.js';
+import Post from './src/components/post.js';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+
+export default class App extends Component {
+  render(){
+      const comments =[{
+        nickname: 'Joana Elena Silva',
+        comment: 'Excelente Foto!'
+      }, {
+        nickname: 'Rafael Gustavo Pereira',
+        comment: 'Muito ruim! Faço Melhor....'
+      }]
+    return (
+      <View style={{flex:1}}>
+        <Header/>
+        <Post image={require('./assets/imgs/fence.jpg')}
+            comments={comments}
+        />
+  
+      </View>
+    )
+  }
+  
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
